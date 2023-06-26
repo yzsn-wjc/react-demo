cef2f82738b24e0f72cbadcd47603d300d3e1d97
wangjincai <wangjincai@rainbowcn.com>
2023-06-26 15:10:24 +0800

    feat:应用通信

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { unstable_HistoryRouter as Router } from "react-router-dom";
 import { ConfigProvider } from "antd";
+import { initGlobalState } from "qiankun";
 
 import zhCN from "antd/locale/zh_CN";
 import "antd/dist/reset.css";
@@ -21,3 +22,15 @@ root.render(
     </ConfigProvider>
   </Router>
 );
+
+const initState = { a: 111, b: 222 };
+
+// 初始化 state
+const actions = initGlobalState(initState);
+
+actions.onGlobalStateChange((state, prev) => {
+  // state: 变更后的状态; prev 变更前的状态
+  console.log(state, prev, "------");
+});
+actions.setGlobalState(initState);
+// actions.offGlobalStateChange();
